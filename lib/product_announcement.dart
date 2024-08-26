c9a3cb3e87c6f4ebb3d134c8de8dbc4289851039
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-08-26 16:36:24 -0300

    refactor(app): Creating methods to organize dynamic data rendering

--- lib/product_announcement.dart
+++ lib/product_announcement.dart
@@ -41,78 +41,13 @@ class ProductAnnouncement extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Builder(builder: (context) {
-                      if (discount > 0) {
-                        return Chip(
-                          side: BorderSide.none,
-                          label: Text(
-                            "$discount% OFF",
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 10,
-                            ),
-                          ),
-                          backgroundColor: primary,
-                          padding: const EdgeInsets.all(0),
-                          labelPadding: const EdgeInsets.symmetric(
-                              vertical: -6, horizontal: 6),
-                          visualDensity:
-                              const VisualDensity(horizontal: 0, vertical: 0),
-                          shape: const RoundedRectangleBorder(
-                              side: BorderSide.none,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(4))),
-                        );
-                      } else {
-                        return const Text("");
-                      }
-                    }),
+                    showDiscountSign(discount),
                     Text("$name - $brand",
                         style: const TextStyle(
                             color: dark,
                             fontSize: 16,
                             fontWeight: FontWeight.w400)),
-                    Builder(builder: (context) {
-                      if (discount > 0) {
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              NumberFormat.simpleCurrency(
-                                      locale: 'pt-BR', decimalDigits: 2)
-                                  .format(price),
-                              style: const TextStyle(
-                                  color: secondary,
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: 14,
-                                  decoration: TextDecoration.lineThrough),
-                            ),
-                            Text(
-                              NumberFormat.simpleCurrency(
-                                      locale: 'pt-BR', decimalDigits: 2)
-                                  .format(priceWithDiscount),
-                              style: const TextStyle(
-                                color: dark,
-                                fontWeight: FontWeight.w400,
-                                fontSize: 20,
-                              ),
-                            )
-                          ],
-                        );
-                      } else {
-                        return Text(
-                          NumberFormat.simpleCurrency(
-                                  locale: 'pt-BR', decimalDigits: 2)
-                              .format(price),
-                          style: const TextStyle(
-                            color: dark,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20,
-                          ),
-                        );
-                      }
-                    }),
+                    showPrice(price, discount, priceWithDiscount),
                     Text(
                       "Em até $installmentsQuantity de ${NumberFormat.simpleCurrency(locale: 'pt-BR', decimalDigits: 2).format(installmentsPrice)}",
                       style: const TextStyle(
@@ -121,25 +56,90 @@ class ProductAnnouncement extends StatelessWidget {
                         fontSize: 14,
                       ),
                     ),
-                    Builder(builder: (context) {
-                      if (isNew) {
-                        return const Text(
-                          "NOVO",
-                          style: TextStyle(
-                            color: secondary,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 12,
-                          ),
-                        );
-                      } else {
-                        return const Text("");
-                      }
-                    }),
+                    showNewSign(isNew)
                   ],
                 ),
               )
             ],
           ),
         ));
+  }
+}
+
+showDiscountSign(discount) {
+  if (discount > 0) {
+    return Chip(
+      side: BorderSide.none,
+      label: Text(
+        "$discount% OFF",
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w700,
+          fontSize: 10,
+        ),
+      ),
+      backgroundColor: primary,
+      padding: const EdgeInsets.all(0),
+      labelPadding: const EdgeInsets.symmetric(vertical: -6, horizontal: 6),
+      visualDensity: const VisualDensity(horizontal: 0, vertical: 0),
+      shape: const RoundedRectangleBorder(
+          side: BorderSide.none,
+          borderRadius: BorderRadius.all(Radius.circular(4))),
+    );
+  } else {
+    return const Text("");
+  }
+}
+
+showPrice(price, discount, priceWithDiscount) {
+  if (discount > 0) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          NumberFormat.simpleCurrency(locale: 'pt-BR', decimalDigits: 2)
+              .format(price),
+          style: const TextStyle(
+              color: secondary,
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+              decoration: TextDecoration.lineThrough),
+        ),
+        Text(
+          NumberFormat.simpleCurrency(locale: 'pt-BR', decimalDigits: 2)
+              .format(priceWithDiscount),
+          style: const TextStyle(
+            color: dark,
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+          ),
+        )
+      ],
+    );
+  } else {
+    return Text(
+      NumberFormat.simpleCurrency(locale: 'pt-BR', decimalDigits: 2)
+          .format(price),
+      style: const TextStyle(
+        color: dark,
+        fontWeight: FontWeight.w400,
+        fontSize: 20,
+      ),
+    );
+  }
+}
+
+showNewSign(isNew) {
+  if (isNew) {
+    return const Text(
+      "NOVO",
+      style: TextStyle(
+        color: secondary,
+        fontWeight: FontWeight.w400,
+        fontSize: 12,
+      ),
+    );
+  } else {
+    return const Text("");
   }
 }
