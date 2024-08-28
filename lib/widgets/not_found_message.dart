5d33c3e80f6ad045689c7014f60609a819401ae2
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-08-28 10:52:54 -0300

    fix(app): Fixing not found message centralization

--- lib/widgets/not_found_message.dart
+++ lib/widgets/not_found_message.dart
@@ -11,35 +11,35 @@ class NotFoundMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 79, left: 69, right: 69, bottom: 20),
-          child: Image.asset("images/not_found.png", width: 236, height: 180),
-        ),
-        const Center(
-          child: Padding(
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 79, left: 69, right: 69, bottom: 20),
+            child: Image.asset("images/not_found.png", width: 236, height: 180),
+          ),
+          const Padding(
             padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 10),
             child: Text(
               "Resultado não encontrado",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: dark),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 10),
-          child: Text(
-            'Não encontramos nenhum resultado parecido com "$notFoundProductName".',
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w400,
-              color: Color(0XFF4a6569)
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 10),
+            child: Text(
+              'Não encontramos nenhum resultado parecido com "$notFoundProductName".',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Color(0XFF4a6569)
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
