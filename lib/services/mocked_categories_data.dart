102e95dfd440d4c9a77b416b41eee7e34d48f822
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-08-28 00:03:08 -0300

    feat(app): Creating categories list

---
+++ lib/services/mocked_categories_data.dart
@@ -0,0 +1,15 @@
+const categories = [
+  "Anestésicos e Agulhas Gengival",
+  "Biossegurança",
+  "Descartáveis",
+  "Dentística e Estética",
+  "Ortodontia",
+  "Endodontia",
+  "Periféricos e Peças de Mão",
+  "Moldagem",
+  "Prótese",
+  "Cimentos",
+  "Instrumentos",
+  "Cirurgia e Periodontia",
+  "Radiologia"
+];
